7da60f5cf035196405c41f30b1d6fde7aa94f459
Francesco Vasco <fsco_v-github@yahoo.it>
2018-02-16 18:47:35 +0100

    soluzione proposta

--- src/main/java/web/SensorsJava.java
+++ src/main/java/web/SensorsJava.java
@@ -1,44 +1,50 @@
 package web;
 
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
+
 import dao.ControlUnit;
 import dao.Database;
 import dao.IotClient;
 import data.Sensor;
+import data.SensorConfiguration;
+import data.SensorType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.publisher.ParallelFlux;
 
-import java.util.Map;
-
 @RestController
-@SuppressWarnings("unused")
 public class SensorsJava {
-
-    @Autowired
-    private Database database;
 
     @Autowired
     private ControlUnit controlUnit;
 
     @Autowired
+    private Database database;
+
+    @Autowired
     private IotClient iotClient;
 
-    /**
-     * Return all sensors
-     */
     @GetMapping("/sensorsJava")
     public Flux<Sensor> allSensors() {
-        throw new UnsupportedOperationException();
+        return controlUnit.getSensors();
     }
 
-    /**
-     * Return only sensor with valid (finite) value
-     */
     @GetMapping("/sensorsJava/active")
     public ParallelFlux<Sensor> countActive() {
-        throw new UnsupportedOperationException();
+        return controlUnit.getSensors()
+                          .parallel()
+                          .flatMap(sensor -> iotClient.query(sensor)
+                                                      .take(1)
+                                                      .flatMap(d -> (d.isNaN()) ? Mono.empty() : Mono.fromSupplier(() -> sensor)));
     }
 
     /**
@@ -46,7 +52,75 @@ public class SensorsJava {
      */
     @GetMapping("/sensorsJava/check")
     public ParallelFlux<Map.Entry<Sensor, Double>> checkSensor() {
-        throw new UnsupportedOperationException();
+        Mono<Map<SensorTypeContext, SensorConfiguration>> rangeMap = database.loadConfiguration()
+                                                                             .collectMap(SensorsJava::configToSTC)
+                                                                             .cache();
+
+        Mono.from(rangeMap).subscribe(rm -> System.out.println("fetched configuration.. : " + rm));
+
+        return controlUnit.getSensors()
+                          .parallel()
+                          .flatMap(sensor -> {
+                              return iotClient.query(sensor)
+                                              .take(3)
+                                              .filter(d -> !d.isNaN())
+                                              .collectList()
+                                              .map(list -> (list.isEmpty()) ? Double.NaN : list.stream().collect(Collectors.averagingDouble(d -> d)))
+                                              .filterWhen(d -> Mono.from(rangeMap).<Boolean> map(rm -> !rm.get(new SensorTypeContext(sensor.getType(), sensor.getContext())).getValidRange().contains(d)))
+                                              .map(d -> new AbstractMap.SimpleEntry<>(sensor, d));
+            }
+        );
+    }
+
+    public static SensorTypeContext configToSTC(SensorConfiguration config) {
+        return new SensorTypeContext(config.getSensorType(), config.getContext());
+    }
+
+    public static class SensorTypeContext {
+
+        private final SensorType sensorType;
+        private final String context;
+
+        public SensorTypeContext(SensorType sensorType, String context) {
+            this.sensorType = sensorType;
+            this.context = context;
+        }
+
+        public SensorType getSensorType() {
+            return sensorType;
+        }
+
+        public String getContext() {
+            return context;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((context == null) ? 0 : context.hashCode());
+            result = prime * result + ((sensorType == null) ? 0 : sensorType.hashCode());
+            return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            SensorTypeContext other = (SensorTypeContext) obj;
+            if (context == null) {
+                if (other.context != null)
+                    return false;
+            } else if (!context.equals(other.context))
+                return false;
+            if (sensorType != other.sensorType)
+                return false;
+            return true;
+        }
+
     }
 }
-
--- src/main/kotlin/web/SensorsKt.kt
+++ src/main/kotlin/web/SensorsKt.kt
@@ -4,12 +4,16 @@ import dao.ControlUnit
 import dao.Database
 import dao.IotClient
 import data.Sensor
+import kotlinx.coroutines.experimental.future.await
+import kotlinx.coroutines.experimental.reactive.awaitFirst
+import kotlinx.coroutines.experimental.reactive.awaitSingle
+import mapNotNull
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Flux
 import reactor.core.publisher.ParallelFlux
+import java.util.*
 
-@Suppress("unused")
 @RestController
 class SensorsKt(
         private val database: Database,
@@ -20,18 +24,44 @@ class SensorsKt(
      * Return all sensors
      */
     @GetMapping("/sensorsKt")
-    fun allSensors(): Flux<Sensor> = TODO()
+    fun allSensors(): Flux<Sensor> = controlUnit.getSensors()
 
 
     /**
      * Return only sensor with valid (finite) value
      */
     @GetMapping("/sensorsKt/active")
-    fun countActive(): ParallelFlux<Sensor> = TODO()
+    fun countActive(): ParallelFlux<Sensor> = controlUnit
+            .getSensors()
+            .parallel()
+            .mapNotNull { sensor ->
+                val value = iotClient.query(sensor).awaitFirst()
+                sensor.takeIf { value.isFinite() }
+            }
 
     /**
      * Associate each sensor with the average over three values out of range.
      */
     @GetMapping("/sensorsKt/check")
-    fun checkSensor(): ParallelFlux<Map.Entry<Sensor, Double>> = TODO()
+    fun checkSensor(): ParallelFlux<Map.Entry<Sensor, Double>> {
+        val rangeMapFuture = database.loadConfiguration()
+                .collectMap({ it.sensorType to it.context }, { it.validRange })
+                .toFuture()
+
+        return controlUnit
+                .getSensors()
+                .parallel()
+                .mapNotNull { sensor ->
+                    val average = iotClient.query(sensor)
+                            .take(3)
+                            .collectList()
+                            .awaitSingle()
+                            .filter { it.isFinite() }
+                            .average()
+                    val rangeMap = rangeMapFuture.await()
+                    if (average !in rangeMap.getValue(sensor.type to sensor.context))
+                        AbstractMap.SimpleEntry(sensor, average)
+                    else null
+                }
+    }
 }
